cab183ea195688ea334f293c9d0d60fcbf19de5f
Patryk Wianecki <wianek17@gmail.com>
2020-02-09 19:51:11 +0100

    Added location validator tests

--- location-api/src/main/java/com/app/validator/LocationValidator.java
+++ location-api/src/main/java/com/app/validator/LocationValidator.java
@@ -10,7 +10,8 @@ import com.app.model.LocationResponse;
 public class LocationValidator {
 
   public void validateLocationResponse(LocationResponse locationResponse) {
-    Optional.ofNullable(locationResponse).orElseThrow(() -> new NullPointerException("Missing location response"));
+    Optional.ofNullable(locationResponse)
+        .orElseThrow(() -> new NullPointerException("Missing location response"));
 
     String countyName = locationResponse.getCountyName();
     String localityName = locationResponse.getLocalityName();
@@ -34,26 +35,4 @@ public class LocationValidator {
   private static boolean isNotNameUpperCase(String name) {
     return !name.equals(name.toUpperCase());
   }
-
-  //    public boolean validateGeometry(final Geometry geometry) {
-  //        return geometry != null && validateCoordinates(geometry.getCoordinates());
-  //    }
-  //
-  //    public boolean validateProperties(final Properties properties) {
-  //        return !isBlank(properties.getTitle());
-  //    }
-  //
-  //    private static boolean validateCoordinates(final List<Double> coordinates) {
-  //        return CollectionUtils.isNotEmpty(coordinates) &&
-  //            validateLongitude(Optional.ofNullable(coordinates.get(0)).orElse(181.0)) &&
-  //            validateLatitude(Optional.ofNullable(coordinates.get(1)).orElse(-91.0));
-  //    }
-  //
-  //    private static boolean validateLongitude(final Double longitude) {
-  //        return longitude != null && longitude <= 180 && longitude >= -180;
-  //    }
-  //
-  //    private static boolean validateLatitude(final Double latitude) {
-  //        return latitude != null && latitude <= 90 && latitude >= -90;
-  //    }
 }
--- location-api/src/test/java/com/app/validator/LocationValidatorTest.java
+++ location-api/src/test/java/com/app/validator/LocationValidatorTest.java
@@ -5,20 +5,93 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.app.model.LocationResponse;
+
+import static com.app.data.MockDataForTests.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(SpringExtension.class)
 class LocationValidatorTest {
 
   @InjectMocks
   private LocationValidator locationValidator;
 
+  private LocationResponse response = createValidLocationResponse();
+
   @Test
-  void should() {
+  void should_validate_response_without_throwing_any_exception() {
+    // when + then
+    locationValidator.validateLocationResponse(response);
+  }
+
+  @Test
+  void should_throw_exception_when_response_is_null() {
     // given
 
-    // when
-    // TODO
+    // when + then
+    assertThrows(NullPointerException.class, () -> locationValidator.validateLocationResponse(null));
+  }
 
-    // then
+  @Test
+  void should_throw_exception_when_voivodeship_name_is_null() {
+    // given
+    response.setVoivodeshipName(null);
 
+    // when + then
+    assertThrows(IllegalArgumentException.class, () -> locationValidator.validateLocationResponse(response));
+  }
+
+  @Test
+  void should_throw_exception_when_voivodeship_name_has_wrong_format() {
+    // given
+    response.setVoivodeshipName(response.getVoivodeshipName().toLowerCase());
+
+    // when + then
+    assertThrows(IllegalArgumentException.class, () -> locationValidator.validateLocationResponse(response));
+  }
+
+  @Test
+  void should_throw_exception_when_county_name_is_null() {
+    // given
+    response.setCountyName(null);
+
+    // when + then
+    assertThrows(IllegalArgumentException.class, () -> locationValidator.validateLocationResponse(response));
+  }
+
+  @Test
+  void should_throw_exception_when_county_name_has_wrong_format() {
+    // given
+    response.setCountyName(response.getCountyName().toLowerCase());
+
+    // when + then
+    assertThrows(IllegalArgumentException.class, () -> locationValidator.validateLocationResponse(response));
+  }
+
+  @Test
+  void should_throw_exception_when_locality_name_is_null() {
+    // given
+    response.setLocalityName(null);
+
+    // when + then
+    assertThrows(IllegalArgumentException.class, () -> locationValidator.validateLocationResponse(response));
+  }
+
+  @Test
+  void should_throw_exception_when_locality_name_has_wrong_format() {
+    // given
+    response.setLocalityName(response.getLocalityName().toLowerCase());
+
+    // when + then
+    assertThrows(IllegalArgumentException.class, () -> locationValidator.validateLocationResponse(response));
+  }
+
+  @Test
+  void should_throw_exception_when_radius_is_not_greater_than_zero() {
+    // given
+    response.setRadius(0);
+
+    // when + then
+    assertThrows(IllegalArgumentException.class, () -> locationValidator.validateLocationResponse(response));
   }
 }
