01ce891cd32b5895efe674c0754f82c2441ce693
Patryk Wianecki <wianek17@gmail.com>
2020-03-01 23:46:47 +0100

    Tidied up

--- otomoto-service/src/main/java/com/app/controller/ExceptionController.java
+++
@@ -1,9 +0,0 @@
-package com.app.controller;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ExceptionController {
-
-//    @ExceptionHandler
-}
