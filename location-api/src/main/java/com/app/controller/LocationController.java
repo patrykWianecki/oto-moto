f86badb0a26c3e48332cb6f2390565e3af74d643
Patryk Wianecki <wianek17@gmail.com>
2020-03-01 23:45:28 +0100

    Tidied up

--- location-api/src/main/java/com/app/controller/LocationController.java
+++ location-api/src/main/java/com/app/controller/LocationController.java
@@ -2,10 +2,8 @@ package com.app.controller;
 
 import java.util.List;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +15,8 @@ import com.app.validator.LocationValidator;
 
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.http.MediaType.*;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/localities")
@@ -25,7 +25,7 @@ public class LocationController {
   private final LocationService locationService;
   private final LocationValidator locationValidator;
 
-  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(produces = APPLICATION_JSON_VALUE)
   public ResponseEntity<List<LocalityDto>> getAvailableLocalities(@RequestBody LocationResponse locationResponse) {
     locationValidator.validateLocationResponse(locationResponse);
     List<LocalityDto> localities = locationService.createRequest(locationResponse);
