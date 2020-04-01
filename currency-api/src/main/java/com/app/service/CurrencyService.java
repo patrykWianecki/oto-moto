e3736a92db58066e335e8acc69d4f7749145cc2b
Patryk Wianecki <wianek17@gmail.com>
2020-04-01 23:31:55 +0200

    Injected exchange api url in field

--- currency-api/src/main/java/com/app/service/CurrencyService.java
+++ currency-api/src/main/java/com/app/service/CurrencyService.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.app.model.Currency;
@@ -22,7 +23,8 @@ import com.google.gson.GsonBuilder;
 @Service
 public class CurrencyService {
 
-  private final static String PATH = "https://api.exchangeratesapi.io/latest?base=PLN";
+  @Value("${exchange-api-url}")
+  private String EXCHANGE_API_URL;
 
   public Currency findCurrentExchange() throws URISyntaxException, InterruptedException {
     CountDownLatch countDownLatch = new CountDownLatch(1);
@@ -42,16 +44,16 @@ public class CurrencyService {
         .orElseThrow(() -> new NullPointerException("Failed to get actual currency"));
   }
 
-  private static HttpRequest createGetRequest() throws URISyntaxException {
+  private HttpRequest createGetRequest() throws URISyntaxException {
     return HttpRequest.newBuilder()
-        .uri(new URI(PATH))
+        .uri(new URI(EXCHANGE_API_URL))
         .version(Version.HTTP_2)
         .timeout(Duration.ofSeconds(10))
         .GET()
         .build();
   }
 
-  private static CompletableFuture<HttpResponse<String>> createResponse() throws URISyntaxException {
+  private CompletableFuture<HttpResponse<String>> createResponse() throws URISyntaxException {
     return HttpClient.newBuilder()
         .proxy(ProxySelector.getDefault())
         .build()
