d11a5f5f2bfcdb6e4982047513a2134174cef3b0
Patryk Wianecki <wianek17@gmail.com>
2020-04-16 22:57:09 +0200

    Removed redundant annotation

--- location-api/src/main/java/com/app/LocationApiApplication.java
+++ location-api/src/main/java/com/app/LocationApiApplication.java
@@ -3,10 +3,7 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.RequiredArgsConstructor;
-
 @SpringBootApplication
-@RequiredArgsConstructor
 public class LocationApiApplication {
 
   public static void main(String[] args) {
